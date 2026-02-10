3b4c001d2b23887cb7498eb4afcf3626b7579122
soumajit131292 <soumajit131292@gmail.com>
2026-02-10 16:05:59 +0530

    eks-add-on

--- envs/dev/eks-add-on/alb-controller/main.tf
+++ envs/dev/eks-add-on/alb-controller/main.tf
@@ -1,19 +1,5 @@
-# module "alb_controller" {
-#   source = "../../../modules/eks-alb-controller"
-
-#   cluster_name       = data.terraform_remote_state.eks.outputs.cluster_name
-#   region             = var.region
-#   vpc_id             = data.terraform_remote_state.vpc.outputs.vpc_id
-#   oidc_provider_arn  = data.terraform_remote_state.eks.outputs.oidc_provider_arn
-#   oidc_issuer_url    = data.terraform_remote_state.eks.outputs.oidc_issuer_url
-
-#   tags = {
-#     component = "alb-controller"
-#   }
-# }
-
 module "alb_controller" {
-  source = "../../../modules/eks-alb-controller"
+  source = "../../../../modules/eks-alb-controller"
 
   cluster_name         = data.terraform_remote_state.eks.outputs.cluster_name
   region               = var.region
