e9609bf50280c03679a0e6f2eab74ae1ff7689e6
soumajit131292 <soumajit131292@gmail.com>
2026-02-10 16:21:32 +0530

    eks-add-on

--- modules/eks-alb-controller/outputs.tf
+++ modules/eks-alb-controller/outputs.tf
@@ -1,7 +1,7 @@
-output "iam_role_arn" {
-  description = "IAM role ARN used by ALB controller"
-  value       = aws_iam_role.alb_controller.arn
-}
+# output "iam_role_arn" {
+#   description = "IAM role ARN used by ALB controller"
+#   value       = aws_iam_role.alb_controller.arn
+# }
 
 output "helm_release_status" {
   description = "Helm release status"
