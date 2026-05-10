a74dfbbf8b7e05fc770dde01f7736693b42ee3bd
soumajit131292 <soumajit131292@gmail.com>
2026-05-10 10:29:14 +0530

    Configure prod ArgoCD ALB ingress with explicit subnet discovery

--- envs/prod/eu-west-1/eks-add-on/argocd/data.tf
+++ envs/prod/eu-west-1/eks-add-on/argocd/data.tf
@@ -7,3 +7,25 @@ data "terraform_remote_state" "eks" {
     region = "us-east-1"
   }
 }
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "crave-infra-terraform-state-bucket"
+    key    = "network/prod/eu-west-1/vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_subnets" "alb_public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.terraform_remote_state.vpc.outputs.vpc_id]
+  }
+
+  tags = {
+    "kubernetes.io/role/elb"                                                        = "1"
+    "kubernetes.io/cluster/${data.terraform_remote_state.eks.outputs.cluster_name}" = "shared"
+  }
+}
