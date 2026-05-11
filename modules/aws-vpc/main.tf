47d7de9c486bb77146dfc2e9e41734fe2f51045c
soumajit131292 <soumajit131292@gmail.com>
2026-05-11 20:35:49 +0530

    Update prod terraform configs and VPC module

--- modules/aws-vpc/main.tf
+++ modules/aws-vpc/main.tf
@@ -447,29 +447,29 @@ resource "aws_subnet" "firewall" {
 }
 
 
-resource "aws_security_group" "public_test" {
-  name   = "${var.name}-public-test-sg"
-  vpc_id = aws_vpc.this.id
+# resource "aws_security_group" "public_test" {
+#   name   = "${var.name}-public-test-sg"
+#   vpc_id = aws_vpc.this.id
 
-  ingress {
-    description = "SSH from local machine"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["49.207.193.197/32"]
-  }
+#   ingress {
+#     description = "SSH from local machine"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["49.207.193.197/32"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = merge(var.tags, {
-    Name = "${var.name}-public-test-sg"
-  })
-}
+#   tags = merge(var.tags, {
+#     Name = "${var.name}-public-test-sg"
+#   })
+# }
 
 
 ###############################
